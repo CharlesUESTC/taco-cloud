453b12b4fa7f619c03fa4a85125882dd576d81a0
Weidong Chen <weidong_98@126.com>
2023-04-21 16:28:29 +0800

    displaying information

--- src/main/java/com/pierre/tacos/web/HomeController.java
+++ src/main/java/com/pierre/tacos/web/HomeController.java
@@ -1,4 +1,4 @@
-package com.pierre.tacos;
+package com.pierre.tacos.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/test/java/com/pierre/tacos/web/HomeControllerTest.java
+++ src/test/java/com/pierre/tacos/web/HomeControllerTest.java
@@ -1,4 +1,4 @@
-package com.pierre.tacos;
+package com.pierre.tacos.web;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
